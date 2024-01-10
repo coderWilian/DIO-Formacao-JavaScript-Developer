428635a773edf398949d069fd160e3dea81ec1a9
skywilian <codewilian852@gmail.com>
2024-01-10 13:22:15 -0300

    Problema 1 resolvido

---
+++ src/exercicios/exer01.js
@@ -0,0 +1,21 @@
+/**
+ * Faça um programa que receba média do aluno:
+ * Caso a média seja < 5 Imprima "Reprovado"
+ * Caso a média seja >= 5 e < 7 Imprima "Recuperação"
+ * Caso a média seja >= 7 Imprima "Aprovado"
+ */
+
+
+const { gets, print } = require("./funcoes-auxiliares")
+
+let average = gets()
+
+if (average < 5) {
+  resultado = "Reprovado"
+} else if (average >= 5 && average < 7) {
+  resultado = "Recuperação"
+} else {
+  resultado = "Aprovado"
+}
+
+print(resultado)
